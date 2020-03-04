77d191e640fbddc84e7abb6c5244a9b10b7cc474
shihab <echoshihab@gmail.com>
2020-03-04 18:32:43 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ConceptEFCoreAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
