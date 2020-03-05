efeacfd423c69a61769331215fd1415642dfc968
shihab <echoshihab@gmail.com>
2020-03-04 22:44:06 -0500

    add models, views, controllers  + Seed data

---
+++ Data/SchoolContext.cs
@@ -0,0 +1,30 @@
+﻿using ConceptEFCoreAPP.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConceptEFCoreAPP.Data
+{
+    public class SchoolContext : DbContext
+    {
+       public SchoolContext(DbContextOptions<SchoolContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        //tables are created with singlular table with logic below
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+
+        }
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConceptEFCoreAPP.Models
+{
+    public class Course
+    {
+        //this allows for us to insert primary key instead getting database to generate t
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set;}
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
