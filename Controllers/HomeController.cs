304ef989741b44681ffafae34574e001ffc8a311
shihab <echoshihab@gmail.com>
2020-03-06 21:34:05 -0500

    add group by enrollment date action and view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,22 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ConceptEFCoreAPP.Models;
+using ConceptEFCoreAPP.Data;
+using ConceptEFCoreAPP.Models.SchoolViewModel;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace ConceptEFCoreAPP.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly SchoolContext _context;
+        public HomeController(SchoolContext context, ILogger<HomeController> logger)
         {
+            _context = context;
             _logger = logger;
         }
+
+
 
         public IActionResult Index()
         {
@@ -26,6 +31,19 @@ namespace ConceptEFCoreAPP.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
