580f7582ebf6c1fcc0f27a3e08e72efac5cfd6bb
Shihab Khan <echoshihab@gmail.com>
2020-03-10 16:07:40 -0400

    add+modify migration for default values

--- Migrations/20200310195847_ComplexDataModel.Designer.cs
+++ Migrations/20200310195847_ComplexDataModel.Designer.cs
@@ -4,14 +4,16 @@ using ConceptEFCoreAPP.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ConceptEFCoreAPP.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20200310195847_ComplexDataModel")]
+    partial class ComplexDataModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
